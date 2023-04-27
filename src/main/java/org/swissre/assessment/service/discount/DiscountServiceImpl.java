92921fec762a75c07fcfaf54a27bf80128ea48d2
Mátyás <matyas.ember@gmail.com>
2023-04-27 11:07:04 +0200

    Streams refactor.

--- src/main/java/org/swissre/assessment/service/discount/DiscountServiceImpl.java
+++ src/main/java/org/swissre/assessment/service/discount/DiscountServiceImpl.java
@@ -1,16 +1,23 @@
 package org.swissre.assessment.service.discount;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.summingInt;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 import static org.swissre.assessment.service.menu.MenuUtil.flattenOrder;
 
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
+import java.util.Objects;
+import java.util.stream.IntStream;
 import org.swissre.assessment.domain.MenuItem;
 import org.swissre.assessment.domain.OrderItem;
 import org.swissre.assessment.domain.Type;
@@ -29,7 +36,7 @@ public class DiscountServiceImpl implements DiscountService {
     List<MenuItem> extras = flattedOrderList.stream()
         .filter(menuItem -> menuItem.getType() == Type.EXTRA)
         .sorted(Comparator.comparing(MenuItem::getPrice).reversed())
-        .collect(Collectors.toList());
+        .collect(toList());
 
     Iterator<MenuItem> iterator = extras.iterator();
 
@@ -57,70 +64,57 @@ public class DiscountServiceImpl implements DiscountService {
       Map<Integer, List<OrderItem>> allOrders) {
 
     List<SimpleImmutableEntry<Integer, OrderItem>> extOrds = extractOrders(allOrders);
-    List<SimpleImmutableEntry<Integer, MenuItem>> extOrdsWithDuplicates = splitOrders(extOrds);
-    List<SimpleImmutableEntry<Integer, MenuItem>> extractedOrdersDiscounted = new ArrayList<>();
+    List<SimpleImmutableEntry<Integer, MenuItem>> extOrdsWithReps = splitOrders(extOrds);
+    List<SimpleImmutableEntry<Integer, MenuItem>> extOrdsDisc = filterDiscounts(extOrdsWithReps);
 
-    filterDiscounts(extOrdsWithDuplicates, extractedOrdersDiscounted);
-
-    Map<Integer, List<OrderItem>> discountedOrdersMap = convertBack(extractedOrdersDiscounted);
+    Map<Integer, List<OrderItem>> discountedOrdersMap = convertBack(extOrdsDisc);
 
     return discountedOrdersMap.getOrDefault(orderId, new ArrayList<>());
   }
 
   private Map<Integer, List<OrderItem>> convertBack(
       List<SimpleImmutableEntry<Integer, MenuItem>> extOrdsDisc) {
-    return extOrdsDisc.stream().collect(Collectors.groupingBy(Entry::getKey,
-            Collectors.mapping(Entry::getValue, Collectors.toList()))).entrySet().stream()
-        .collect(Collectors.toMap(Entry::getKey, menuItemEntry -> {
-
-          List<MenuItem> menuItems = menuItemEntry.getValue();
-          return convertMenuItemsToOrderItems(menuItems);
-        }));
+    return extOrdsDisc.stream().collect(groupingBy(Entry::getKey,
+            mapping(Entry::getValue, toList()))).entrySet().stream()
+        .collect(toMap(Entry::getKey,
+            menuItemEntry -> convertMenuItemsToOrderItems(menuItemEntry.getValue())));
   }
 
   private List<OrderItem> convertMenuItemsToOrderItems(List<MenuItem> menuItems) {
     Map<String, Integer> menuItemOccurrences = menuItems.stream()
-        .collect(Collectors.groupingBy(
-            MenuItem::getCode, LinkedHashMap::new, Collectors.summingInt(e -> 1)));
+        .collect(groupingBy(MenuItem::getCode, LinkedHashMap::new, summingInt(e -> 1)));
 
     return menuItemOccurrences.entrySet().stream()
         .map(menuItemOcc -> new OrderItem(MenuItem.getMenuItemByCode(menuItemOcc.getKey()),
             menuItemOcc.getValue()))
-        .collect(Collectors.toList());
+        .collect(toList());
   }
 
-  private void filterDiscounts(
-      List<SimpleImmutableEntry<Integer, MenuItem>> extractedOrdersWithDuplicates,
-      List<SimpleImmutableEntry<Integer, MenuItem>> extractedOrdersDiscounted) {
-    int i = 1;
-    for (SimpleImmutableEntry<Integer, MenuItem> menuItemEntry : extractedOrdersWithDuplicates) {
-      if (menuItemEntry.getValue().getType() == Type.BEVERAGE) {
-        if (i % 5 == 0) {
-          extractedOrdersDiscounted.add(menuItemEntry);
-        }
-        i++;
-      }
-    }
+  private List<SimpleImmutableEntry<Integer, MenuItem>> filterDiscounts(
+      List<SimpleImmutableEntry<Integer, MenuItem>> extrOrdersWithReps) {
+
+    List<SimpleImmutableEntry<Integer, MenuItem>> beverages = extrOrdersWithReps.stream()
+        .filter(menuItemEntry -> menuItemEntry.getValue().getType() == Type.BEVERAGE)
+        .collect(toList());
+
+    return IntStream.range(0, beverages.size())
+        .mapToObj(i -> (i + 1) % 5 == 0 ? beverages.get(i) : null)
+        .filter(Objects::nonNull).collect(toList());
   }
 
   private List<SimpleImmutableEntry<Integer, MenuItem>> splitOrders(
       List<SimpleImmutableEntry<Integer, OrderItem>> extractedOrders) {
-    List<SimpleImmutableEntry<Integer, MenuItem>> extractedOrdersWithDuplicates = new ArrayList<>();
-    extractedOrders.forEach(order -> {
-      int i = order.getValue().getQuantity();
-      while (i > 0) {
-        extractedOrdersWithDuplicates.add(
-            new SimpleImmutableEntry<>(order.getKey(), order.getValue().getMenuItem()));
-        i--;
-      }
-    });
-    return extractedOrdersWithDuplicates;
+    return extractedOrders.stream().map(ordItemInt ->
+            IntStream.range(0, ordItemInt.getValue().getQuantity()).mapToObj(i ->
+                    new SimpleImmutableEntry<>(ordItemInt.getKey(), ordItemInt.getValue().getMenuItem()))
+                .collect(toList()))
+        .flatMap(Collection::stream).collect(toList());
   }
 
   private List<SimpleImmutableEntry<Integer, OrderItem>> extractOrders(
       Map<Integer, List<OrderItem>> allOrders) {
     return allOrders.entrySet().stream()
         .flatMap(e -> e.getValue().stream().map(v -> new SimpleImmutableEntry<>(e.getKey(), v)))
-        .collect(Collectors.toList());
+        .collect(toList());
   }
 }
