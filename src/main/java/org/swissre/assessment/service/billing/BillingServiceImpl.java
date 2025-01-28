0890a6bfb8cf57c96af5a200b43c81f45070d2f1
Matyas <matyas.ember@gmail.com>
2025-01-28 13:16:39 +0100

    Code cleanup

--- src/main/java/org/swissre/assessment/service/billing/BillingServiceImpl.java
+++ src/main/java/org/swissre/assessment/service/billing/BillingServiceImpl.java
@@ -15,32 +15,23 @@ public class BillingServiceImpl implements BillingService {
 
   @Override
   public BigDecimal calcSum(List<OrderItem> order) {
-    return order.stream().map(orderItem -> {
-      BigDecimal price = orderItem.getMenuItem().getPrice();
-      BigDecimal quantity = new BigDecimal(String.valueOf(orderItem.getQuantity()));
-
-      return price.multiply(quantity);
-    }).reduce(ZERO, BigDecimal::add);
+    return order.stream().map(BillingServiceImpl::getSumPrice).reduce(ZERO, BigDecimal::add);
   }
 
   @Override
   public BigDecimal calcSumWithDisc(List<OrderItem> order, List<OrderItem> discOrderItems) {
     return normalizedOrder(order).stream().map(orderItem -> {
-      BigDecimal price = orderItem.getMenuItem().getPrice();
-      BigDecimal quantity = new BigDecimal(String.valueOf(orderItem.getQuantity()));
-      BigDecimal sumPrice = price.multiply(quantity);
-
+      BigDecimal sumPrice = getSumPrice(orderItem);
       Optional<OrderItem> discounted = findInDiscountedList(discOrderItems, orderItem);
 
-      return discounted.map(
-              item -> {
-                BigDecimal disPrice = item.getMenuItem().getPrice();
-                BigDecimal disQuantity = new BigDecimal(String.valueOf(item.getQuantity()));
-
-                return sumPrice.subtract(disPrice.multiply(disQuantity));
-              })
-          .orElse(sumPrice);
+      return discounted.map(item -> calculateDiscountedPrice(item, sumPrice)).orElse(sumPrice);
     }).reduce(ZERO, BigDecimal::add);
+  }
+
+  private static BigDecimal calculateDiscountedPrice(OrderItem item, BigDecimal sumPrice) {
+    BigDecimal disPrice = item.getMenuItem().getPrice();
+    BigDecimal disQuantity = new BigDecimal(String.valueOf(item.getQuantity()));
+    return sumPrice.subtract(disPrice.multiply(disQuantity));
   }
 
   private Optional<OrderItem> findInDiscountedList(List<OrderItem> disOrderItems,
@@ -52,11 +43,17 @@ public class BillingServiceImpl implements BillingService {
 
   private List<OrderItem> normalizedOrder(List<OrderItem> orders) {
     Map<String, Integer> menuItems = orders.stream()
-        .collect(Collectors.groupingBy(orderItem -> orderItem.getMenuItem().getCode(),
-            LinkedHashMap::new, Collectors.summingInt(OrderItem::getQuantity)));
+            .collect(Collectors.groupingBy(orderItem -> orderItem.getMenuItem().getCode(),
+                    LinkedHashMap::new, Collectors.summingInt(OrderItem::getQuantity)));
 
     return menuItems.entrySet().stream()
-        .map(MenuItemConverter::convertToOrderItem)
-        .collect(Collectors.toList());
+            .map(MenuItemConverter::convertToOrderItem)
+            .collect(Collectors.toList());
+  }
+
+  private static BigDecimal getSumPrice(OrderItem orderItem) {
+    BigDecimal price = orderItem.getMenuItem().getPrice();
+    BigDecimal quantity = new BigDecimal(String.valueOf(orderItem.getQuantity()));
+    return price.multiply(quantity);
   }
 }
